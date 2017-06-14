c1415f51d888bf08044e20ff21b99e2eb1918f27
holyshared <holy.shared.design@gmail.com>
2017-06-14 20:00:06 +0900

    add views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:line]
+         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => %i(line)
 end
